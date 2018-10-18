cf115bd351ff70e33ef237043cf9b04e7c90dcb6
Ralph Bacon <arduino@jargongeneration.uk>
2018-10-18 17:10:49 +0100

    Simple demo sketch

---
+++ AutoSwitchOff.ino
@@ -0,0 +1,19 @@
+#define powerPin A0
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(powerPin, OUTPUT);
+  digitalWrite(powerPin, HIGH);
+  Serial.println("Setup completed");
+}
+
+void loop() {
+  static unsigned int countDown = 10;
+  while (countDown > 0) {
+    Serial.println(countDown);
+    delay(1000);
+    countDown--;
+  }
+
+  digitalWrite(powerPin, LOW);
+}
